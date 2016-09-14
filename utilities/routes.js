ea50b91ffe9c1a2955552cb8036caa471c6bd3f7
James Macon <jtmacon36@gmail.com>
2016-09-13 21:52:22 -0400

    getting rid of some stupid space

--- utilities/routes.js
+++ utilities/routes.js
@@ -3,8 +3,7 @@ var http = require('http');
 routes = {};
 
 //Homepage Routing
-routes.home = function (req, res) 
-{
+routes.home = function (req, res) {
     res.writeHead(200, {'Content-Type' : 'text/plain'});
     res.write('Header\n');
     res.write('Body\n');
@@ -12,10 +11,8 @@ routes.home = function (req, res)
 }
 
 //Route Filter
-module.exports.trafficCop = function (req, res) 
-{
-    if (req.url == '/')
-    {
+module.exports.trafficCop = function (req, res) {
+    if (req.url == '/') {
         routes.home(req, res);
     }
 }
--- utilities/server.js
+++ utilities/server.js
@@ -2,8 +2,7 @@ var http = require('http');
 var routes = require('./routes.js');
 
 //Set Server Info and export to module
-function setInfo (url = '127.0.0.1', protocol = 'http', port = '1337') 
-{
+function setInfo (url = '127.0.0.1', protocol = 'http', port = '1337') {
    var self = this;
    self.url = url;
    self.protocol = protocol;
@@ -16,18 +15,14 @@ var info = setInfo();
 module.exports.info = info;
 
 //Start Server
-module.exports.startEngine = function () 
-{
-    try
-    {
-        http.createServer(function (req, res) 
-        {
+module.exports.startEngine = function () {
+    try {
+        http.createServer(function (req, res) {
             routes.trafficCop(req, res);
         }).listen(info.port, info.url);
         console.log('Server running @ ' + info.protocol + '//' + info.url + ':' + info.port);
     }
-    catch (err)
-    {
+    catch (err) {
         console.error('Unexpected error starting server: ' + err.message);
     }
 }
